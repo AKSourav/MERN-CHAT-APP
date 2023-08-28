c0e989f5a082dbc11c396505122849d4923fc5f7
Anupam Kumar Sourav <anupam.sony32@gmail.com>
2023-08-28 11:24:06 +0530

    fix

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ const server=app.listen(PORT,console.log(`Server Started on PORT ${PORT}`.yellow
 const io= require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000"
+        origin: "*"
     }
 });
 
